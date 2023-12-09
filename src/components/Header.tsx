f93cd9249371c257b3c8ea6219502e15e20719e5
Oleg Ershov <oecorango@gmail.com>
2023-12-10 01:43:34 +0400

    feat: add header and footer components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,29 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { Logo } from './svg/LogoSvg';
+import styles from './Header.module.scss';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <header>
-        <NavLink to={'/'}>Logo</NavLink>
-        <NavLink to={'login'}>LogIn</NavLink>
-        <NavLink to={'registration'}>SignUp</NavLink>
-      </header>
-    </>
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo} onClick={() => navigate('/')}>
+          <p>RSGr</p>
+          <Logo />
+          <p>phQL</p>
+        </div>
+        <div className={styles.navigation}>
+          <select name="lang">
+            <option value="ru">ru</option>
+            <option value="en">en</option>
+          </select>
+          <NavLink to={'/'}>Main</NavLink>
+          <NavLink to={'welcome'}>Welcome</NavLink>
+          <NavLink to={'login'}>LogIn</NavLink>
+          <NavLink to={'registration'}>SignUp</NavLink>
+        </div>
+      </div>
+    </header>
   );
 };
