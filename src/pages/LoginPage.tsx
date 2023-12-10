aec98292378e7d240859203f66e88e7618fe506a
Yahor Tsytsarkin <egormail88@gmail.com>
2023-12-11 00:30:44 +0300

    feat: add PrivateRoute in routes

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,23 +1,7 @@
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { LoginForm } from '../components/LoginForm';
 import styles from './LoginPage.module.scss';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
-  // const navigate = useNavigate();
-  // const auth = getAuth();
-  // const [user, setUser] = useState(auth.currentUser);
-
-  // useEffect(() => {
-  //   auth.onAuthStateChanged((currentUser) => {
-  //     setUser(currentUser);
-  //     if (user) {
-  //       navigate('/');
-  //     }
-  //   });
-  // }, [auth]);
-
   return (
     <div className={styles.container}>
       <h1>Login</h1>
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,31 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, useNavigate } from 'react-router-dom';
 import { RootLayout } from '../layouts/RootLayout';
 import { LoginPage } from '../pages/LoginPage';
 import { ErrorPage } from '../pages/ErrorPage';
 import { MainPage } from '../pages/MainPage';
 import { RegistrationPage } from '../pages/RegistrationPage';
 import { WelcomePage } from '../pages/WelcomePage';
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { PrivateRouteProps } from '../types/interface';
+
+function PrivateRoute({ element }: PrivateRouteProps) {
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+      if (user) {
+        navigate('/');
+      }
+    });
+  }, [auth, navigate, user]);
+
+  return user ? <MainPage /> : element;
+}
 
 export const router = createBrowserRouter([
   {
@@ -13,8 +34,16 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <MainPage /> },
-      { path: '/login', element: <LoginPage /> },
-      { path: '/registration', element: <RegistrationPage /> },
+      {
+        path: '/login',
+        element: <PrivateRoute path="/login" element={<LoginPage />} />,
+      },
+      {
+        path: '/registration',
+        element: (
+          <PrivateRoute path="/registration" element={<RegistrationPage />} />
+        ),
+      },
       { path: '/welcome', element: <WelcomePage /> },
     ],
   },
