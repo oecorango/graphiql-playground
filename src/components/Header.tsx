1b0cfdd5809beebdcd8eccd940251bcdedcc1f13
Oleg Ershov <oecorango@gmail.com>
2023-12-25 13:11:31 +0400

    feat: add sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import { Logo } from './svg/LogoSvg';
 import styles from './Header.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Navigation } from './Navigation';
 
 export const Header = () => {
   const navigate = useNavigate();
   const [curLanguage, setCurLanguage] = useState(localStorage.getItem('lang'));
 
+  const [offset, setOffset] = useState<number>(0);
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY);
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.wrapper}>
+    <header
+      className={
+        offset ? `${styles.header} ${styles.active}` : `${styles.header}`
+      }
+    >
+      <div
+        className={
+          offset ? `${styles.wrapper} ${styles.active}` : `${styles.wrapper}`
+        }
+      >
         <div className={styles.logo} onClick={() => navigate('/')}>
           <p>RSGr</p>
           <Logo />
