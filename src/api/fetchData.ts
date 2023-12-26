2b1f568d6121d11532a19e7295d6aaa3921f37ae
Oleg Ershov <oecorango@gmail.com>
2023-12-26 22:01:30 +0400

    feat: add variable input to query

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -4,22 +4,15 @@ export const fetchData = async (
   url: string,
   headers: HeadersInit,
   query: string,
-  variables: Record<string, string>
+  variables: Record<string, string | number | NonNullable<unknown>>
 ) => {
   try {
-    // const variablesObj = JSON.parse(variables);
-
-    // for (const key in variablesObj) {
-    //   console.log(variablesObj[key]);
-    // }
-
-    // console.log(query);
-
     const response = await fetch(url ? url : RICK_URL, {
       method: 'POST',
       headers,
       body: JSON.stringify({
-        query,
+        query: query,
+        variables: variables,
       }),
     });
     return await response.json();
