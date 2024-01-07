ea7eb93ff3db315d1e4a3a4d8679c6c3fcff65ce
Oleg Ershov <oecorango@gmail.com>
2024-01-07 20:23:33 +0400

    fix: fix translation errors

--- src/schema/loginSchema.ts
+++ src/schema/loginSchema.ts
@@ -1,19 +1,36 @@
 import * as yup from 'yup';
-import { translateText } from '../utils/translateText';
+import { TRANSLATE_TEXT } from '../language/translationOptions';
 
-export const loginSchema = yup
+export const loginSchemaEn = yup
   .object()
   .shape({
     email: yup
       .string()
-      .email(translateText('errorEmail'))
-      .required(translateText('required')),
+      .email(TRANSLATE_TEXT.errorEmail.en)
+      .required(TRANSLATE_TEXT.required.en),
     password: yup
       .string()
       .matches(
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{"':;?/><,.[\]|`~\-\\]).{8,}$/,
-        translateText('errorPassword')
+        TRANSLATE_TEXT.errorPassword.en
       )
-      .required(translateText('required')),
+      .required(TRANSLATE_TEXT.required.en),
   })
-  .required(translateText('required'));
+  .required(TRANSLATE_TEXT.required.en);
+
+export const loginSchemaRu = yup
+  .object()
+  .shape({
+    email: yup
+      .string()
+      .email(TRANSLATE_TEXT.errorEmail.ru)
+      .required(TRANSLATE_TEXT.required.ru),
+    password: yup
+      .string()
+      .matches(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{"':;?/><,.[\]|`~\-\\]).{8,}$/,
+        TRANSLATE_TEXT.errorPassword.ru
+      )
+      .required(TRANSLATE_TEXT.required.ru),
+  })
+  .required(TRANSLATE_TEXT.required.ru);
